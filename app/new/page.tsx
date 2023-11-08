b61cfe70630d370a77c79ef726ef3ad2644ffa8f
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-11-08 06:45:58 +0000

    fix form

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -26,43 +26,45 @@ async function createEnv(formData: FormData) {
 
 export default async function Home() {
   return (
-    <div className="p-8">
-      <form action={createEnv}>
-        <div className="mb-6">
-          <label className="block mb-2 text-sm font-medium text-gray-900">Name</label>
-          <input
-            type="text"
-            name="name"
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block mb-2 text-sm font-medium text-gray-900">Branch</label>
-          <input
-            type="text"
-            name="branch"
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block mb-2 text-sm font-medium text-gray-900">URL</label>
-          <input
-            type="text"
-            name="url"
-            maxLength={20}
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-        >
-          Create
-        </button>
-      </form>
+    <div className="flex justify-center p-8">
+      <div className="flex-1 max-w-screen-xl">
+        <form action={createEnv}>
+          <div className="mb-6">
+            <label className="block mb-2 text-sm font-medium text-gray-900">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-sm font-medium text-gray-900">Branch</label>
+            <input
+              type="text"
+              name="branch"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-sm font-medium text-gray-900">URL</label>
+            <input
+              type="text"
+              name="url"
+              maxLength={20}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 }

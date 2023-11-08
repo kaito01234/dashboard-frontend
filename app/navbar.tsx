bf9e69abb5a3c270c3b4111a2ce4d0fa4b9d5755
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-11-08 00:46:02 +0000

    fix header

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="bg-gray-900 border-gray-200">
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+      <div className="flex flex-wrap justify-between items-center mx-auto px-12 py-4">
         <Link href={'/'} className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Dashborad</span>
         </Link>

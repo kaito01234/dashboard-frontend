137642ca822d30ee24b01d0d45107080cac74ad6
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-11-15 09:50:32 +0000

    fix param

--- app/components/tbody.tsx
+++ app/components/tbody.tsx
@@ -8,13 +8,13 @@ import { DetailModal } from './detailModal';
 
 export type TableData = {
   id: string;
-  name: string;
+  env_name: string;
   branch: string;
   url: string;
-  envStatus: string;
-  e2e: string;
+  env_status: string;
+  e2e_status: string;
   priority: string;
-  createData: string;
+  create_data: string;
 };
 
 type Props = { deleteModal: boolean; detailModal: boolean; tableData: TableData; url1_name: string; url2_name: string };
@@ -27,7 +27,7 @@ export default function TableBody({ deleteModal, detailModal, tableData, url1_na
           <Image className="h-full w-full rounded-full object-cover object-center" src={localImage} alt="image" />
         </div>
       </td>
-      <td className="px-6 py-4 font-medium text-gray-900">{tableData.name}</td>
+      <td className="px-6 py-4 font-medium text-gray-900">{tableData.env_name}</td>
       <td className="px-6 py-4 font-medium">
         <a target="_blank" href={`https://github.com/${process.env.REPO_NAME}/tree/${tableData.branch}`}>
           {tableData.branch}
@@ -44,38 +44,38 @@ export default function TableBody({ deleteModal, detailModal, tableData, url1_na
         </div>
       </td>
       <td className="px-6 py-4">
-        <Status status={tableData.envStatus} />
+        <Status status={tableData.env_status} />
       </td>
       <td className="px-6 py-4">
-        <E2EStatus status={tableData.e2e} />
+        <E2EStatus status={tableData.e2e_status} />
       </td>
       <td className="px-6 py-4 font-medium items-center gap-4">
         <div className="flex justify-end object-right">
           <Link href={`/?detail=${tableData.id}`} className="px-6 text-blue-500">
             Detail
           </Link>
-          {detailModal && <DetailModal id={tableData.id} priority={tableData.priority} createData={tableData.createData} />}
+          {detailModal && <DetailModal id={tableData.id} priority={tableData.priority} createData={tableData.create_data} />}
           <Link
             style={{
               pointerEvents:
-                tableData.envStatus === EnvStatus.Creating ||
-                tableData.envStatus === EnvStatus.Running ||
-                tableData.envStatus === EnvStatus.Failure
+                tableData.env_status === EnvStatus.Creating ||
+                tableData.env_status === EnvStatus.Running ||
+                tableData.env_status === EnvStatus.Failure
                   ? 'all'
                   : 'none',
             }}
             href={`/?delete=${tableData.id}`}
             className={
-              tableData.envStatus === EnvStatus.Creating ||
-              tableData.envStatus === EnvStatus.Running ||
-              tableData.envStatus === EnvStatus.Failure
+              tableData.env_status === EnvStatus.Creating ||
+              tableData.env_status === EnvStatus.Running ||
+              tableData.env_status === EnvStatus.Failure
                 ? 'px-6 text-red-500'
                 : 'px-6 text-gray-300'
             }
           >
             Delete
           </Link>
-          {deleteModal && <DeleteModal id={tableData.id} name={tableData.name} url={tableData.url} />}
+          {deleteModal && <DeleteModal id={tableData.id} name={tableData.env_name} url={tableData.url} />}
         </div>
       </td>
     </tr>
